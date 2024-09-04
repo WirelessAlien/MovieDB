1609e848301fc79d89670c859f921fbb279095b1
WirelessAlien <121420261+WirelessAlien@users.noreply.github.com>
2024-09-04 15:17:40 +0530

    upgrade gradle, sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.wirelessalien.android.moviedb"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.wirelessalien.android.moviedb"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 5
         versionName = "0.9"
 
@@ -38,6 +38,7 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        buildConfig = true
     }
     kotlinOptions {
         jvmTarget = "17"
@@ -46,24 +47,26 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("com.google.code.gson:gson:2.10.1")
-    implementation("androidx.recyclerview:recyclerview:1.3.1")
-    implementation("androidx.browser:browser:1.5.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.browser:browser:1.8.0")
     implementation("androidx.palette:palette:1.0.0")
     implementation("com.google.android.flexbox:flexbox:3.0.0")
-    implementation("androidx.work:work-runtime:2.8.1")
-    implementation("androidx.preference:preference:1.2.0")
+    implementation("androidx.work:work-runtime:2.9.1")
+    implementation("androidx.preference:preference:1.2.1")
+
+    implementation("androidx.core:core-splashscreen:1.0.1")
 
     implementation ("de.hdodenhof:circleimageview:3.1.0")
     implementation("com.squareup.picasso:picasso:2.8")
 
 
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
