2c4373262fba3b6cb2f1aa43971de39ab0192abf
WirelessAlien <121420261+WirelessAlien@users.noreply.github.com>
2024-10-27 21:36:34 +0530

    v1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.wirelessalien.android.moviedb"
         minSdk = 24
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.0"
+        versionCode = 8
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
