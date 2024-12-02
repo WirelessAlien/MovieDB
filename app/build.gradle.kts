e636546f508eb5eac18f373c60ad08ee75126ea3
WirelessAlien <121420261+WirelessAlien@users.noreply.github.com>
2024-12-02 11:03:23 +0530

    minify-enabled false

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
