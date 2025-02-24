7f4ab049181a74300e5df4e2eee31ff85b2ab27e
WirelessAlien <121420261+WirelessAlien@users.noreply.github.com>
2025-02-25 01:07:04 +0530

    access token (trakt)

---
+++ app/src/main/java/com/wirelessalien/android/moviedb/data/TktTokenResponse.kt
@@ -0,0 +1,28 @@
+/*
+ *     This file is part of "ShowCase" formerly Movie DB. <https://github.com/WirelessAlien/MovieDB>
+ *     forked from <https://notabug.org/nvb/MovieDB>
+ *
+ *     Copyright (C) 2024  WirelessAlien <https://github.com/WirelessAlien>
+ *
+ *     ShowCase is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     ShowCase is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with "ShowCase".  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.wirelessalien.android.moviedb.data
+
+data class TktTokenResponse(
+    val accessToken: String,
+    val refreshToken: String,
+    val expiresIn: Long,
+    val createdAt: Long
+)
