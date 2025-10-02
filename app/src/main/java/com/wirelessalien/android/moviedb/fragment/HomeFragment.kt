66c4e57e820db229c76d8ee20a4c30ccf531bbf3
WirelessAlien <121420261+WirelessAlien@users.noreply.github.com>
2025-10-02 18:41:27 +0530

    fix crash

--- app/src/main/java/com/wirelessalien/android/moviedb/fragment/HomeFragment.kt
+++ app/src/main/java/com/wirelessalien/android/moviedb/fragment/HomeFragment.kt
@@ -512,13 +512,20 @@ class HomeFragment : BaseFragment() {
                     adapter.notifyDataSetChanged()
                     if (trendingArrayList.isNotEmpty()) {
                         binding.trendingViewPager.post {
-                            val layoutManager = binding.trendingViewPager.layoutManager as CarouselLayoutManager
-                            val centerView = snapHelper.findSnapView(layoutManager)
-                            if (centerView != null) {
-                                val movieImage = centerView.findViewById<ImageView>(R.id.movieImage)
-                                val zoomIn = AnimationUtils.loadAnimation(requireContext(), R.anim.zoom_in)
-                                movieImage.startAnimation(zoomIn)
-                                centeredView = centerView
+                            if (view?.isAttachedToWindow == true) {
+                                val layoutManager =
+                                    binding.trendingViewPager.layoutManager as CarouselLayoutManager
+                                val centerView = snapHelper.findSnapView(layoutManager)
+                                if (centerView != null) {
+                                    val movieImage =
+                                        centerView.findViewById<ImageView>(R.id.movieImage)
+                                    val zoomIn = AnimationUtils.loadAnimation(
+                                        requireContext(),
+                                        R.anim.zoom_in
+                                    )
+                                    movieImage.startAnimation(zoomIn)
+                                    centeredView = centerView
+                                }
                             }
                         }
                     }
