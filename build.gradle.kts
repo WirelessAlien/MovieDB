9dd457040cb6742d833fff815d13952c83c02237
Abhilash <121420261+WirelessAlien@users.noreply.github.com>
2026-07-21 18:52:12 +0530

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,4 +22,5 @@
 plugins {
     id("com.android.application") version "9.2.1" apply false
     id("com.android.library") version "9.2.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.4.0" apply false
 }
