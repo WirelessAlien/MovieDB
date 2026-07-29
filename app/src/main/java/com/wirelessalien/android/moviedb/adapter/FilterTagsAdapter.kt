ec37a976213352a4c5da04ff9ff817ee5e2f234d
Abhilash <121420261+WirelessAlien@users.noreply.github.com>
2026-07-29 23:04:26 +0530

    fix

--- app/src/main/java/com/wirelessalien/android/moviedb/adapter/FilterTagsAdapter.kt
+++ app/src/main/java/com/wirelessalien/android/moviedb/adapter/FilterTagsAdapter.kt
@@ -23,9 +23,6 @@ package com.wirelessalien.android.moviedb.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.RadioButton
-import android.widget.RadioGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.wirelessalien.android.moviedb.R
 import com.wirelessalien.android.moviedb.data.Tag
